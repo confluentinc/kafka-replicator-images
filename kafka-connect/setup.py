f19b8bea69da1bff2905a123ed5455a62435e7f6
Alex Ayars <aayars@users.noreply.github.com>
2018-04-18 10:17:40 -0700

    pip.req is not needed (#6)

--- kafka-connect/setup.py
+++ kafka-connect/setup.py
@@ -1,10 +1,5 @@
-#!/usr/bin/env python
-
-from pip.req import parse_requirements
 import setuptools
 
-# Filters out relative/local requirements (i.e. ../lib/utils)
-remote_requirements = '\n'.join(str(r.req) for r in parse_requirements("requirements.txt", session='dummy') if r.req)
 
 setuptools.setup(
     name='kafka-connect-tests',
@@ -17,7 +12,7 @@ setuptools.setup(
 
     url="https://github.com/confluentinc/kafka-connect-images",
 
-    install_requires=remote_requirements,
+    dependency_links=open("requirements.txt").read().split("\n"),
 
     packages=['test'],
 
